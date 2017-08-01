cea3c05f0b638e0962e1f17b7f081dad9c76aeee
Mitchell Blom <mitchellblom@gmail.com>
2017-08-01 11:06:51 -0500

    through number 4

---
+++ non_usa_customers.sql
@@ -0,0 +1,1 @@
+select c.firstname, c.lastname, c.customerId, c.country from customer c where c.country != "USA";
---
+++ sales_agents.sql
@@ -0,0 +1,1 @@
+select e.firstname from employee e where e.title = "Sales Support Agent";
